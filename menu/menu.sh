40253bf94649413f4b494b562a2a812add2bf430
sallxd <92096819+sallxd@users.noreply.github.com>
2022-03-12 18:45:44 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -141,7 +141,7 @@ echo -e "
 [${green}00${NC}]${color1} •${color3}$bd Back to exit Menu \033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m"
 echo ""
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m                     ⇱ SALLVPN PROJECT ⇲                      \E[0m"
+echo -e "\E[44;1;39m                     ⇱ PREMIUM BY SALLVPN ⇲                   \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e  ""
  read -p "  Select menu :  " menu
